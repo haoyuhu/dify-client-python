5ec8ad73a464da9c67c286ac30304a1647a97e75
huhaoyu.hahahu <huhaoyu.hahahu@bytedance.com>
2024-06-05 22:28:53 +0800

    feat: update version to 1.0.1
    Change-Id: I5bf32c91f63795af5b9f176ca188208b2c5c8141

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="dify-client-python",
-    version="1.0.0",
+    version="1.0.1",
     author="haoyuhu",
     author_email="im@huhaoyu.com",
     description="A package for interacting with the Dify Service-API",
